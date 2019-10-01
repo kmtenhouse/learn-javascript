eda51bcd78344ae1090a87be5a3077c54cff6cff
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-09-30 17:01:24 -0700

    Adding reference stuff

--- reference/objects-are-by-reference.js
+++ reference/objects-are-by-reference.js
@@ -60,15 +60,3 @@ That means if you want to make truly separate, brand new objects that don't impa
  */
 
 
-/* 
-========================================================
-One way to create a brand new ARRAY with the contents of the original is to use the new ES6 array function Map...
-========================================================
-*/
-const fleetwoodMac = ["Mick Fleetwood", "John McVie", "Stevie Nicks"];
-const sideProject = fleetwoodMac.map(member => member);
-sideProject.push("Christine McVie");
-//this one logs [ 'Mick Fleetwood', 'John McVie', 'Stevie Nicks' ]
-console.log(fleetwoodMac); 
-//this one logs [ 'Mick Fleetwood', 'John McVie', 'Stevie Nicks', 'Christine McVie' ]
-console.log(sideProject); 
